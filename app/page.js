4fa3e15403bd85a33cc98b26d0267995e2027d70
DrissHaddadi <driss.haddadi@outlook.fr>
2023-09-22 10:02:41 +0200

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen ">
+      <div className=" h-screen w-full flex flex-col items-center justify-between bg-zinc-900 ">
+        <h1 className=" text-cyber-yellow ">Hello Cyberpunk 2077</h1>
+        </div>
+    </main>
+  )
+}
