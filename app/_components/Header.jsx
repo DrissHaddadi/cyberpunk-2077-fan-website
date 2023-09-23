bf2c1b8393b655421e2b0cbde429369dc9a40783
DrissHaddadi <driss.haddadi@outlook.fr>
2023-09-23 22:15:50 +0200

    Header updated

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,20 +1,26 @@
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 const Header = () => {
+  const pathname = usePathname()
+  
   return (
     <header>
-    <nav className=" text-cyber-blue p-4">
-      <ul className="flex justify-center space-x-4">
-        <li>
-          <a href="/">Home</a>
+    <nav className=" bg-cyber-yellow text-black text-xl font-medium p-8 flex justify-center">
+      <ul className="flex justify-center h-auto uppercase border-2 border-black rounded-tl-lg rounded-br-lg">
+        <li className={` px-8 py-4 hover:bg-black hover:text-cyber-yellow link ${pathname === '/' ? 'active bg-black text-cyber-yellow' : ''}`}>
+          <Link  href="/">Home</Link>
         </li>
-        <li>
-          <a href="/night-city">Night City</a>
+        <li className={` px-8 py-4 hover:bg-black hover:text-cyber-yellow link ${pathname === '/night-city' ? 'active bg-black text-cyber-yellow' : ''}`}>
+          <Link  href="/night-city">Night City</Link>
         </li>
-        <li>
-          <a href="/corporations">Corporations</a>
+        <li className={` px-8 py-4 hover:bg-black hover:text-cyber-yellow link ${pathname === '/corporations' ? 'active bg-black text-cyber-yellow' : ''}`}>
+          <Link  href="/corporations">Corporations</Link>
         </li>
-        <li>
-          <a href="/gangs">Gangs</a>
+        <li className={` px-8 py-4 hover:bg-black hover:text-cyber-yellow link ${pathname === '/gangs' ? 'active bg-black text-cyber-yellow' : ''}`}>
+          <Link  href="/gangs">Gangs</Link>
         </li>
       </ul>
     </nav>
